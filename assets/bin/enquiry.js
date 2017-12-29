8fe92ec911c790b039207eab34e651f49bd753a7
DenisCaeyers <denis.caeyers@gmail.com>
2017-12-29 11:39:55 +0100

    init commit

---
+++ assets/bin/enquiry.js
@@ -0,0 +1,8 @@
+// DOCUMENT READY
+$(document).ready(function() {
+    console.log("enquiry.js loaded");
+});
+
+function GoToQuestion1(){
+    window.location.href = "http://localhost:3000/question.html";
+}
---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+// Variables
+var server = {
+    host: 'localhost',
+    port: '8001',
+    https: false
+}
+
+// Global Packages
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+
+// Stylesheet Packages
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+// Webserver packages
+var browserSync = require('browser-sync').create();
+
+// Stylesheet Tasks
+gulp.task('sass-dev', function () {
+    return gulp.src([
+        './assets/dev/scss/*.scss',
+      ])
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./assets/bin/cache')) 
+});
+
+// Post CSS Task
+gulp.task('post-css',['sass-dev'], function () {
+    return gulp.src([
+        './assets/bin/cache/*.css',
+      ])
+      .pipe(postcss([ autoprefixer() ]))
+      .pipe(gulp.dest('./assets/bin'))
+      .pipe(browserSync.stream());      
+});
+
+// Javascript Tasks
+gulp.task('js-dev', function () {
+    return gulp.src([
+        './assets/dev/js/*.js',
+      ])
+      .pipe(gulp.dest('./assets/bin'))
+      .pipe(browserSync.stream());      
+});
+
+// Watch Tasks
+gulp.task('watch',['post-css', 'js-dev'], function () {
+    browserSync.init({
+        server: "."
+    });
+    gulp.watch('./assets/dev/scss/**/*.scss', ['post-css']);
+    gulp.watch('./assets/dev/js/**/*.js', ['js-dev']);
+    gulp.watch("./**/*.html").on('change', browserSync.reload);
+});
+
+//Default task
+gulp.task('default', ['watch']);
